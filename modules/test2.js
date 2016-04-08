560a84b2f2fb451372d1e54220239a30e88eae73
fufufufu <flywangh@gmail.com>
2016-04-08 15:03:51 +0800

    add test function

--- modules/test2.js
+++ modules/test2.js
@@ -2,8 +2,8 @@ var fs = require('fs');
 var soap = require('soap');
 var settings = require('../settings');
 var path = require('path');
-//var url = settings.zhcg_webservice_url;
-var url = 'http://localhost:8004/wsdltest?wsdl';
+var url = settings.zhcg_webservice_url;
+//var url = 'http://localhost:8004/wsdltest?wsdl';
 
 //test only
 //xml_req = fs.readFileSync(path.join(__dirname,'request_test.xml'), 'utf-8');
@@ -20,14 +20,22 @@ soap.createClient(url, function(err, client) {
 
 
     xml_req = fs.readFileSync('./rev.xml', 'utf-8');//taskdispatch
+    xml_req_rollback = fs.readFileSync('./rollback.xml', 'utf-8');//taskdispatch
     //xml_req_1 = fs.readFileSync('./request_reply_delay.xml', 'utf-8');
     //xml_req_2 = fs.readFileSync('./request_reply_hang.xml', 'utf-8');
     /*测试新建任务*/
-    client.process({SPID: settings.zhcg_user, SPPWD: settings.zhcg_passwd, func: 'TaskDispatch', request: xml_req}, function (err, res) {
+    //client.process({SPID: settings.zhcg_user, SPPWD: settings.zhcg_passwd, func: 'TaskDispatch', request: xml_req}, function (err, res) {
+    //    if (err) throw err;
+    //
+    //    console.log(res);
+    //});
+//测试回退
+    client.process({SPID: settings.zhcg_user, SPPWD: settings.zhcg_passwd, func: 'TaskRollBack', request: xml_req_rollback}, function (err, res) {
         if (err) throw err;
 
         console.log(res);
     });
+
     /*测试延期*/
     //client.process({SPID:'12345',SPPWD:'22345',func:'ReplyAccredit',request:xml_req_1},function(err,res){
     //    if (err) throw err;
