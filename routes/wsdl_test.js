235836a132113ff97b2745e7babe169c06282dde
fufufufu <flywangh@gmail.com>
2016-05-13 17:35:20 +0800

    wsdl add

--- routes/wsdl_test.js
+++ routes/wsdl_test.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var util = require('util');
+var path = require('path');
+var fs = require('fs');
 
-
-
-router.post('/wsdlt', function(req, res, next){
+router.post('/wsdl', function(req, res, next){
 
 
     console.log(res.body);
@@ -16,9 +16,9 @@ router.post('/wsdlt', function(req, res, next){
 router.get('/wsdl', function(req, res, next){
 
 
-    console.log(res.query);
+    var xml = require('fs').readFileSync(path.join(path.dirname(__dirname),'zhcg.wsdl'), 'utf8');
+    res.send('sssss');
 
-    res.send('<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" ><soap:Body><undefined:processResponse><request>&lt;ResultCode&gt;0&lt;/ResultCode&gt;&lt;ResultDesc&gt;成功保存信息&lt;/ResultDesc&gt;&lt;ResultMemo&gt;TaskDispatch&lt;/ResultMemo&gt;</request></undefined:processResponse></soap:Body></soap:Envelope>')
 });
 
 
