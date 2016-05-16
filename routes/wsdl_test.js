6b2093f7246f17fa5270c4debf5f83c524079d6a
fufufufu <flywangh@gmail.com>
2016-05-16 09:24:54 +0800

    wsdl test

--- routes/wsdl_test.js
+++ routes/wsdl_test.js
@@ -7,10 +7,8 @@ var fs = require('fs');
 router.post('/wsdl', function(req, res, next){
 
 
-    console.log(res.body);
-
+    console.log(req.body);
     res.set('Content-Type','application/xml');
-
     res.send('<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" ><soap:Body><undefined:processResponse><request>&lt;ResultCode&gt;0&lt;/ResultCode&gt;&lt;ResultDesc&gt;成功保存信息&lt;/ResultDesc&gt;&lt;ResultMemo&gt;TaskDispatch&lt;/ResultMemo&gt;</request></undefined:processResponse></soap:Body></soap:Envelope>')
 });
 
