3eba0af3e72727ba481d5aa0147e6fe6bd676ba2
fufufufufu <flywangh@gmail.com>
2016-05-25 22:28:23 +0800

    wsdl bug fix

--- modules/test.js
+++ modules/test.js
@@ -1,8 +1,11 @@
 var soap = require('soap');
-var url = 'http://101.200.174.136:8004/wsdltest?wsdl';
+var url = 'http://localhost:8004/wsdltest?wsdl';
+var url2 = 'http://183.136.153.50:8996/egovaService?wsdl';
 var args = {name: 'value'};
-soap.createClient(url, function(err, client) {
-    client.process(args, function(err, result) {
-        console.log(err);
-    });
+soap.createClient(url2, function(err, client) {
+    //client.process(args, function(err, result) {
+    //    console.log(err);
+    //});
+
+    console.log(client.describe());
 });
