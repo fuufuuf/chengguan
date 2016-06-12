41bf1a0418ce141a42b3fea4ea5c53d71bde3aa4
fufufufu <flywangh@gmail.com>
2016-06-12 09:10:29 +0800

    add chinese bridge name

--- modules/test.js
+++ modules/test.js
@@ -1,11 +1,3 @@
-var soap = require('soap');
-var url = 'http://localhost:8004/wsdltest?wsdl';
-var url2 = 'http://183.136.153.50:8996/egovaService?wsdl';
-var args = {name: 'value'};
-soap.createClient(url2, function(err, client) {
-    //client.process(args, function(err, result) {
-    //    console.log(err);
-    //});
+var s = {a:{b:'c'}};
 
-    console.log(client.describe());
-});
+console.log(s.a.c);
